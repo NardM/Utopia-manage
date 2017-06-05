b2f3de7253f8801e23bfc275cc7def7b7e8b41ad
nardm <lenar.thg@gmail.com>
2017-06-05 12:33:40 +0300

    new

--- src/app/manager/chat/request-list/request-item-mini/request-item-mini.component.ts
+++ src/app/manager/chat/request-list/request-item-mini/request-item-mini.component.ts
@@ -34,7 +34,6 @@ export class RequestItemMiniComponent implements OnChanges {
     deleteRequestBool: boolean = false;
 
     ngOnChanges() {
-
     }
 
     onChange(event, request: Request, ind: number) {
